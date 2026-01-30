7982c27ae2e0fb66a8e70b1037b60aa38f4720f4
SkinChangerDev <cjt07sfmj@mozmail.com>
2026-01-29 22:03:30 -0600

    Finish first revision of VoidZFSInstall.sh

--- Scripts/VoidZFSInstall.sh
+++ Scripts/VoidZFSInstall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 # script for installing Void Linux (glibc) on a ZFS root with ZFSBootMenu using hrmpf
 # assumes a x86_64 UEFI system
 
@@ -18,28 +18,31 @@ PRIMARYUSER_NAME="skinchanger"
 # xbps mirror to use
 MIRROR="https://mirrors.servercentral.com/voidlinux/current"
 # base package(s) to install
-BASEPACKAGE="base-system micro" # death to vi
+BASEPACKAGE="base-system xtools zfs zfsbootmenu micro" # death to vi
 
 # hostname of the system
-HOSTNAME="foobar"
+INSTALL_HOSTNAME="foobar"
 # glibc locale(s) of the system
-LOCALE="en_US.UTF-8 UTF-8 
+INSTALL_LOCALE="en_US.UTF-8 UTF-8 
 en_US ISO-8859-1"
 # timezone of the system (in /usr/share/zoneinfo)
-TIMEZOME="America/Chicago"
+INSTALL_TIMEZOME="America/Chicago"
 # default keymap of the system
-KEYMAP="us"
+INSTALL_KEYMAP="us"
 
 # TODO:
-# root password
-# primary user name
-# primary user password
+# create primary user
+# set primary user password
 # setup ZFS swap
 # automount ZFS home and bulk dataset
 
+bootstrap_package() {
+    XBPS_ARCH=x86_64 xbps-install -S -R $MIRROR -r /mnt $1
+}
+
 do_warning_start() {
     if [ "$(id -u)" -ne 0 ]; then
-	    echo "this script needs to be run as root"
+        echo "this script needs to be run as root"
         exit 1
     fi
 
@@ -70,6 +73,13 @@ do_rootdisk_prep() {
     sgdisk -n "2:0:-10m" -t "2:bf00" "$ZFSDEVICE"
 }
 
+do_bootdevice_prep() {
+    mkfs.vfat -F32 "$BOOTDEVICE"
+
+    mkdir -p /mnt/boot/efi
+    mount $BOOTDEVICE /mnt/boot/efi
+}
+
 do_zfsdevice_prep() {
     zpool create -f -o ashift=12 \
     -O compression=lz4 \
@@ -89,18 +99,58 @@ do_zfsdevice_prep() {
 
     # for ZFSBootMenu
     zpool set bootfs=$ZPOOLNAME/ROOT/void $ZPOOLNAME
+    zfs set org.zfsbootmenu:commandline="quiet" $ZPOOLNAME/ROOT
 
     zpool export $ZPOOLNAME
     zpool import -N -R /mnt $ZPOOLNAME
     zfs mount $ZPOOLNAME/ROOT/void
     zfs mount $ZPOOLNAME/HOME/default
 
+    # update device symlinks
     udevadm trigger
 }
 
 do_bootstrap() {
-    XBPS_ARCH=x86_64 xbps-install -S -R $MIRROR -r /mnt base-system
+    # base system
+    bootstrap_package $BASEPACKAGE
+
+    # hostid
     cp /etc/hostid /mnt/etc
+
+    # hostname
+    echo "$INSTALL_HOSTNAME" > /mnt/etc/hostname
+    echo "\
+# IPv4 hosts
+127.0.0.1	localhost
+127.0.1.1	${INSTALL_HOSTNAME}
+
+# IPv6 hosts
+::1		localhost ip6-localhost ip6-loopback
+ff02::1		ip6-allnodes
+ff02::2		ip6-allrouters"\
+    > /mnt/etc/hosts
+
+    # keymap and hardware clock
+    echo "$(cat rc.conf)
+KEYMAP=\"${INSTALL_KEYMAP}\"
+HARDWARECLOCK=\"UTC\""\
+    > /mnt/etc/rc.conf
+
+    # timezone
+    ln -sf /mnt/usr/share/zoneinfo/$INSTALL_TIMEZOME /mnt/etc/localtime
+
+    # glibc locale
+    echo "$(cat /mnt/etc/default/libc-locales)
+${INSTALL_LOCALE}"\
+    > /mnt/etc/default/libc-locales
+    xbps-reconfigure -r /mnt -f glibc-locales
+
+    # generate fstab
+    xgenfstab -U /mnt
+
+    # root password
+    echo "set a root password"
+    passwd -R /mnt
 }
 
 # main()
@@ -108,9 +158,16 @@ do_warning_start
 
 zgenhostid
 do_rootdisk_prep
+do_bootdevice_prep
 do_zfsdevice_prep
 do_bootstrap
 
-xchroot /mnt
+xchroot /mnt bash -c '
+xgenfstab
+generate-zbm
+'
 
+umount -n -R /mnt
+zpool export $ZPOOLNAME
 
+echo "All done, ready for first boot!"
